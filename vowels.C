72c79b4af1c992a39481e796508e6d05e14cd7fd
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-02 13:12:45 +0530

    vowels.C

---
+++ vowels.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+char san;
+printf("Input a character\n");
+scanf("%c", &san);
+if ((san >= 'a' && san <= 'z') || (san >= 'A' &&san <= 'Z'))
+{
+if (san=='a' || san=='A' || san=='e' || san=='E' || san=='i' || san=='I' || san=='o' || san=='O' || san== 'u' || san=='U')
+printf("%c is a vowel.\n", san);
+else
+printf("%c is a consonant.\n", san);
+}
+else
+printf("%c is neither a vowel nor a consonant.\n", san);
+return 0;
+}
