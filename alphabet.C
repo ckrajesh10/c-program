76d0354ad3540132108a61f8c5b2a7aa8984c624
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-02 13:27:13 +0530

    alphabet.C

---
+++ alphabet.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if((a>='a' && a<='z')||(a>='A' && a<='Z'))
+    {
+        printf("alphapet");
+    }
+    else
+    {
+        printf("not a alphapet");
+    }
+    return 0;
+    getch();
+    }
