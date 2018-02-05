cec5c306e79cfddc80411dad2d182f09f753a680
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-05 15:52:13 +0530

    sum

---
+++ sum.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=0;i<=0;i++ )
+    {
+        sum += i;  
+    }
+    printf("Sum = %d",sum);
+    getch();
+    
+}
