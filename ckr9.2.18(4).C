3a74dd81f945991b5a2f1d9f9cac8a587adc26b4
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-09 07:57:41 +0000

    Create ckr9.2.18(4).C

---
+++ ckr9.2.18(4).C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conioh>
+int main() 
+{
+	int n,m,i;
+  printf("enter 
+    scanf("%d,%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+    if(i%2!=0)
+    {
+     printf("%d\n",i);
+    }
+    }
+    getch();
+    return 0;
+}
