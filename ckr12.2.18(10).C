c1ac7b40af7b8a6141a65ad487e8e7ee6b68d5ab
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-12 16:28:07 +0530

    Create ckr12.2.18(10).C

---
+++ ckr12.2.18(10).C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+	int n,i,e;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+{ e=n*i;
+printf("%d*%d=%d\n",n,i,e);
+}
+	return 0;
+  getch();
+}
