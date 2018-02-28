bba328ea215275ba0ab7e7e57a40fbc0ce54d0a5
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-02-28 15:44:37 +0530

    Create ckr28.2.18(41).C

---
+++ ckr28.2.18(41).C
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100],count=0;
+	int i;
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]!=' ')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+}
+getch();
+return 0;
+}
