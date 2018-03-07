b3cad65e0b1a4798406e07226d90def3b1f50cd9
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-03-07 15:59:32 +0530

    Create ckr45.C

---
+++ ckr45.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+	long int n,dig=0;
+   scanf("%ld",&n);
+   while(n!=0)
+    { 
+      n=n/10;
+      dig++;
+     }
+printf("%ld",dig);
+      return 0;
+      getch();
+      }
