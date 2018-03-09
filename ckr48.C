917d0af21dd22d4bbc98cd97597d9a48d43669db
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-03-09 13:20:24 +0530

    Create ckr48.C

---
+++ ckr48.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int n,i,a[20],sum;
+float avg;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum+=a[i];
+}
+avg=sum/n;
+printf("%f",avg);
+getch();
+}
