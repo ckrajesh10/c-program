acd4526bced9eaa697eec0d6b51c27d114b5940d
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-03-09 13:15:46 +0530

    Create ckr46.C

---
+++ ckr46.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,next;
+	printf("TO PRINT THE NEXT NUMBER OF THE GIVEN NUMBER\n");
+	scanf("%d",&n);
+	next=n+1;
+	printf("THE NEXT NUMBER OF THE GIVEN NUMBER IS %d\n",next);
+}
+getch();
