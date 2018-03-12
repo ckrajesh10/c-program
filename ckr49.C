b45de74d9740076fdcda927c18edbc390725fa6a
ckrajesh10 <35688063+ckrajesh10@users.noreply.github.com>
2018-03-12 15:49:17 +0530

    Create ckr49.C

---
+++ ckr49.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,c;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d",c);
+	return 0;
+  getch()
+  }
